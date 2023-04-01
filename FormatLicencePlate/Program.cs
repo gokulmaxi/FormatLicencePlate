dfac2f8d00856f5773549cecd530e76fd704e0af
gokulmaxi <gokulakams@gmail.com>
2023-04-01 09:05:05 +0530

    Completed the assesment

--- FormatLicencePlate/Program.cs
+++ FormatLicencePlate/Program.cs
@@ -4,37 +4,36 @@
     {
         static void Main(string[] args)
         {
-            string unFormatedDMV = "2-5g-3-J"; 
-          Console.WriteLine( formatDMV(unFormatedDMV,2));
-        }
-        static string formatDMV(string unFormatedDMV,int groupNo)
-        {
-            string formatedDMV =unFormatedDMV.ToUpper();
-            formatedDMV =unFormatedDMV.Replace("-","");
-            int index = 0;
-            List<string> listGroup = new List<string>();
-            char[] stringArray = formatedDMV.ToCharArray();
-            Array.Reverse(stringArray);
-            string reversedString = new string(stringArray);
-            while (true)        {
-                if (groupNo < (reversedString.Count() - index))
-                {
-                    string subStringGroup = formatedDMV.Substring(index, groupNo);
-                    listGroup.Add(subStringGroup);
-                    index = groupNo + index;
-                } else
-                {
-                    listGroup.Add(reversedString.Substring(index));
-                    break;
-                }
-            }
-            listGroup.Reverse();
-            string finalString ="";
-            foreach (var item in listGroup)
+            string unFormatedDMV = "2-5sd-fg-3-J";
+            Console.WriteLine(formatDMV(unFormatedDMV, 4));
+
+            static string formatDMV(string unFormatedDMV, int groupNo)
             {
-                finalString = item + "-" +finalString ;
+
+                List<string> listGroup = new List<string>();
+                // convert string to upper and reverse the string
+                string upperCaseDMV = unFormatedDMV.ToUpper();
+                string formatedDMV = upperCaseDMV.Replace("-", "");
+                int index = formatedDMV.Count();
+                while (true)
+                {
+                    if (groupNo <  index)
+                    {
+                        index = index - groupNo;
+                        string subStringGroup = formatedDMV.Substring(index, groupNo);
+                        listGroup.Add(subStringGroup);
+                       // index = groupNo + index;
+                    }
+                    else
+                    {
+                        listGroup.Add(formatedDMV.Substring(0,index));
+                        break;
+                    }
+                }
+                listGroup.Reverse();
+                string finalDMV = string.Join("-", listGroup.ToArray());
+                return finalDMV;
             }
-            return finalString;
         }
     }
 }
